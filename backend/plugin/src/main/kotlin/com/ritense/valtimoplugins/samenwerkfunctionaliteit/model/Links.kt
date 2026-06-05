7c7b088a465d5e8a4747d8a94eb424a445b57dcf
furkang-risa <Furkan@risa-it.nl>
2026-06-05 14:24:02 +0200

    Remove comments

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/model/Links.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/model/Links.kt
@@ -3,12 +3,7 @@ package com.ritense.valtimoplugins.samenwerkfunctionaliteit.model
 import com.fasterxml.jackson.annotation.JsonAnyGetter
 import com.fasterxml.jackson.annotation.JsonAnySetter
 
-/**
- * Represents an object whose properties are dynamic and map to Link objects.
- * Serialized/deserialized as a JSON object with arbitrary named properties.
- */
 data class Links(
-    // keep a map of additionalProperties => Link
     private val _additionalProperties: MutableMap<String, Link> = mutableMapOf()
 ) {
     @JsonAnyGetter
