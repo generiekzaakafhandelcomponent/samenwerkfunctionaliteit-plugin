d00fcc208d25071335e93a7e7519724861d33b03
furkang-risa <Furkan@risa-it.nl>
2026-06-05 13:47:28 +0200

    Remove unnecessary JsonProperty, don't include type if null. Operaton throws if type is null.

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/model/Link.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/model/Link.kt
@@ -1,29 +1,15 @@
 package com.ritense.valtimoplugins.samenwerkfunctionaliteit.model
 
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonInclude
 
 data class Link(
-    @JsonProperty("deprecation")
     val deprecation: String? = null,
-
-    @JsonProperty("href")
     val href: String? = null,
-
-    @JsonProperty("hreflang")
     val hreflang: String? = null,
-
-    @JsonProperty("name")
     val name: String? = null,
-
-    @JsonProperty("profile")
     val profile: String? = null,
-
-    @JsonProperty("templated")
     val templated: Boolean? = null,
-
-    @JsonProperty("title")
     val title: String? = null,
-
-    @JsonProperty("type")
+    @field:JsonInclude(JsonInclude.Include.NON_NULL)
     val type: String? = null
 )
