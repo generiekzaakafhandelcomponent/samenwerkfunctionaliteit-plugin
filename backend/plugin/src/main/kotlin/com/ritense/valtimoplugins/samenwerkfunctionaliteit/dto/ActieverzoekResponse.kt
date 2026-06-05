269a5f403d8986e74ec1b4410f6ded41537bc97c
furkang-risa <Furkan@risa-it.nl>
2026-06-05 11:19:23 +0200

    Update model

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/dto/ActieverzoekResponse.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/dto/ActieverzoekResponse.kt
@@ -1,7 +1,64 @@
 package com.ritense.valtimoplugins.samenwerkfunctionaliteit.dto
 
-import com.ritense.valtimoplugins.samenwerkfunctionaliteit.model.Actieverzoek
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.ritense.valtimoplugins.samenwerkfunctionaliteit.model.Document
+import com.ritense.valtimoplugins.samenwerkfunctionaliteit.model.Links
+import com.ritense.valtimoplugins.samenwerkfunctionaliteit.model.Status
+import java.time.OffsetDateTime
+import java.util.UUID
 
 data class ActieverzoekResponse(
-    val actieverzoek: Actieverzoek?
+    @JsonProperty("_links")
+    val links: Links? = null,
+
+    @JsonProperty("aantalBerichten")
+    val aantalBerichten: Int? = null,
+
+    @JsonProperty("actieverzoekId")
+    val actieverzoekId: UUID? = null,
+
+    @JsonProperty("creatieDatumTijd")
+    val creatieDatumTijd: OffsetDateTime? = null,
+
+    @JsonProperty("documenten")
+    val documenten: List<Document>? = null,
+
+    @JsonProperty("laatstAangepastDatumTijd")
+    val laatstAangepastDatumTijd: OffsetDateTime? = null,
+
+    @JsonProperty("laatstAangepastDoor")
+    val laatstAangepastDoor: String? = null, // pattern: \d{20}
+
+    @JsonProperty("laatstAangepastDoorNaam")
+    val laatstAangepastDoorNaam: String? = null,
+
+    @JsonProperty("melding")
+    val melding: String? = null,
+
+    @JsonProperty("omschrijving")
+    val omschrijving: String? = null,
+
+    @JsonProperty("ontvanger")
+    val ontvanger: String? = null, // pattern: \d{20}
+
+    @JsonProperty("ontvangerNaam")
+    val ontvangerNaam: String? = null,
+
+    @JsonProperty("productId")
+    val productId: String? = null,
+
+    @JsonProperty("samenwerkingId")
+    val samenwerkingId: String? = null,
+
+    @JsonProperty("status")
+    val status: Status? = null,
+
+    @JsonProperty("titel")
+    val titel: String? = null,
+
+    @JsonProperty("zender")
+    val zender: String? = null, // pattern: \d{20}
+
+    @JsonProperty("zenderNaam")
+    val zenderNaam: String? = null
 )
