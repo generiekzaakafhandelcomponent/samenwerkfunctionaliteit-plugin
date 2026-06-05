c2c6f55f78f241502ddf69618ad3a95180aab719
furkang-risa <Furkan@risa-it.nl>
2026-06-05 14:23:33 +0200

    Remove JsonProperty and comments

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/model/Actieverzoek.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/model/Actieverzoek.kt
@@ -7,56 +7,22 @@ import java.util.UUID
 data class Actieverzoek(
     @JsonProperty("_links")
     val links: Links? = null,
-
-    @JsonProperty("aantalBerichten")
     val aantalBerichten: Int? = null,
-
-    @JsonProperty("actieverzoekId")
     val actieverzoekId: UUID? = null,
-
-    @JsonProperty("creatieDatumTijd")
     val creatieDatumTijd: OffsetDateTime? = null,
-
-    @JsonProperty("documenten")
     val documenten: List<Document>? = null,
-
-    @JsonProperty("laatstAangepastDatumTijd")
     val laatstAangepastDatumTijd: OffsetDateTime? = null,
-
-    @JsonProperty("laatstAangepastDoor")
-    val laatstAangepastDoor: String? = null, // pattern: \d{20}
-
-    @JsonProperty("laatstAangepastDoorNaam")
+    val laatstAangepastDoor: String? = null,
     val laatstAangepastDoorNaam: String? = null,
-
-    @JsonProperty("melding")
     val melding: String? = null,
-
-    @JsonProperty("omschrijving")
     val omschrijving: String? = null,
-
-    @JsonProperty("ontvanger")
-    val ontvanger: String? = null, // pattern: \d{20}
-
-    @JsonProperty("ontvangerNaam")
+    val ontvanger: String? = null,
     val ontvangerNaam: String? = null,
-
-    @JsonProperty("productId")
     val productId: String? = null,
-
-    @JsonProperty("samenwerkingId")
     val samenwerkingId: String? = null,
-
-    @JsonProperty("status")
     val status: Status? = null,
-
-    @JsonProperty("titel")
     val titel: String? = null,
-
-    @JsonProperty("zender")
-    val zender: String? = null, // pattern: \d{20}
-
-    @JsonProperty("zenderNaam")
+    val zender: String? = null,
     val zenderNaam: String? = null
 )
 
