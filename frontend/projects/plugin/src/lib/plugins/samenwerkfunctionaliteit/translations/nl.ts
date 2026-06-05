0adbd8e33c84c80efb7920e727acc3b8dbf8819e
furkang-risa <Furkan@risa-it.nl>
2026-06-05 09:28:26 +0200

    Remove translations

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/translations/nl.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/translations/nl.ts
@@ -1,8 +1,6 @@
 export const pluginNlTranslations = {
   samenwerkfunctionaliteit: {
-    samenwerkingId: 'Het samenwerkingId van deze actieverzoeken.',
-    actieverzoekId: 'Het actieverzoekId van dit actieverzoek.',
-    isOrganisatieDeOntvanger: 'De actieverzoeken filteren op organisatie.',
+
   }
 };
 
