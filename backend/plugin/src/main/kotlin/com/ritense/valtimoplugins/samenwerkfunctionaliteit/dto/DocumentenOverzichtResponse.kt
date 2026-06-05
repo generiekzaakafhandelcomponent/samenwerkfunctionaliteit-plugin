a33f52209e51641c03f40b69ca259ead529921aa
furkang-risa <Furkan@risa-it.nl>
2026-06-05 16:03:10 +0200

    Update DTO

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/dto/DocumentenOverzichtResponse.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/dto/DocumentenOverzichtResponse.kt
@@ -1,7 +1,16 @@
 package com.ritense.valtimoplugins.samenwerkfunctionaliteit.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.ritense.valtimoplugins.samenwerkfunctionaliteit.model.Document
+import com.ritense.valtimoplugins.samenwerkfunctionaliteit.model.Links
 
 data class DocumentenOverzichtResponse(
-    val documentenOverzicht: List<Document>
+    @JsonProperty("_embedded")
+    val embedded: Documenten? = null,
+    @JsonProperty("_links")
+    val links: Links? = null,
+)
+
+data class Documenten(
+    val documenten: List<Document>
 )
