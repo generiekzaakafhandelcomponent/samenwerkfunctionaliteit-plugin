a813d7d563c52d0808fd0d2cea56d6f256d69e5c
furkang-risa <Furkan@risa-it.nl>
2026-06-08 13:49:02 +0200

    Refactor MutableMap to Map

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/model/Links.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/model/Links.kt
@@ -1,16 +1,10 @@
 package com.ritense.valtimoplugins.samenwerkfunctionaliteit.model
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter
-import com.fasterxml.jackson.annotation.JsonAnySetter
 
 data class Links(
-    private val _additionalProperties: MutableMap<String, Link> = mutableMapOf()
+    private val _additionalProperties: Map<String, Link> = mutableMapOf()
 ) {
     @JsonAnyGetter
     fun additionalProperties(): Map<String, Link> = _additionalProperties
-
-    @JsonAnySetter
-    fun setAdditionalProperty(name: String, value: Link) {
-        _additionalProperties[name] = value
-    }
 }
