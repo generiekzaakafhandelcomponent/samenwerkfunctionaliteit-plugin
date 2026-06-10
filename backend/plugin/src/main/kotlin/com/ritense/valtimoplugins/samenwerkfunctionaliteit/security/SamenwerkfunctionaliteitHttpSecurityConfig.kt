c6d24cdc542a2d67f00bce0f45312a97e1b20f6c
Jasper Kunkeler <jasper@risa-it.nl>
2026-06-10 16:14:01 +0200

    Move HttpSecurityConfig

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/security/SamenwerkfunctionaliteitHttpSecurityConfig.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/security/SamenwerkfunctionaliteitHttpSecurityConfig.kt
@@ -1,4 +1,4 @@
-package com.ritense.valtimoplugins.samenwerkfunctionaliteit.gateway
+package com.ritense.valtimoplugins.samenwerkfunctionaliteit.security
 
 import com.ritense.valtimo.contract.security.config.HttpConfigurerConfigurationException
 import com.ritense.valtimo.contract.security.config.HttpSecurityConfigurer
@@ -7,10 +7,9 @@ import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.stereotype.Component
 
-
 @Order(1)
 @Component
-class SwfHttpSecurityConfig : HttpSecurityConfigurer {
+class SamenwerkfunctionaliteitHttpSecurityConfig : HttpSecurityConfigurer {
     override fun configure(http: HttpSecurity) {
         try {
             http.authorizeHttpRequests { requests ->
