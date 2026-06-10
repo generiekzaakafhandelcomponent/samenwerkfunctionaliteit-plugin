c2a964c816188aa1fd23a3b47c89b030d9ae5cbd
Jasper Kunkeler <jasper@risa-it.nl>
2026-06-10 16:23:59 +0200

    Add feature flag check to HttpSecurityConfig

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/security/SamenwerkfunctionaliteitHttpSecurityConfig.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/security/SamenwerkfunctionaliteitHttpSecurityConfig.kt
@@ -2,6 +2,7 @@ package com.ritense.valtimoplugins.samenwerkfunctionaliteit.security
 
 import com.ritense.valtimo.contract.security.config.HttpConfigurerConfigurationException
 import com.ritense.valtimo.contract.security.config.HttpSecurityConfigurer
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.core.annotation.Order
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component
 
 @Order(1)
 @Component
+@ConditionalOnProperty(prefix = "valtimo.samenwerkfunctionaliteit", name = ["enableGateway"], havingValue = "true")
 class SamenwerkfunctionaliteitHttpSecurityConfig : HttpSecurityConfigurer {
     override fun configure(http: HttpSecurity) {
         try {
