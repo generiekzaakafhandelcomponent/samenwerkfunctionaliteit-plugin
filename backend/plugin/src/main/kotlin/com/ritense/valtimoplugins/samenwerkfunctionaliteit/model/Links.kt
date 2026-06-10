935ff455f38d132bc336aca21f7eec9484ba9d45
furkang-risa <Furkan@risa-it.nl>
2026-06-10 10:51:48 +0200

    Remove DTO functionality in model

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/model/Links.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/model/Links.kt
@@ -4,7 +4,4 @@ import com.fasterxml.jackson.annotation.JsonAnyGetter
 
 data class Links(
     private val _additionalProperties: Map<String, Link> = mutableMapOf()
-) {
-    @JsonAnyGetter
-    fun additionalProperties(): Map<String, Link> = _additionalProperties
-}
+)
