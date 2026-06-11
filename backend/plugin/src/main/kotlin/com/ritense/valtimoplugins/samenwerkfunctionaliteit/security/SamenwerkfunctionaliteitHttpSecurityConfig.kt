e0e7e16be1d26dae690b0447c59af45d8030539e
Jasper Kunkeler <jasper@risa-it.nl>
2026-06-11 13:36:53 +0200

    Update readme

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/security/SamenwerkfunctionaliteitHttpSecurityConfig.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/security/SamenwerkfunctionaliteitHttpSecurityConfig.kt
@@ -1,7 +1,5 @@
 package com.ritense.valtimoplugins.samenwerkfunctionaliteit.security
 
-import com.ritense.valtimo.contract.authentication.AuthoritiesConstants.ADMIN
-import com.ritense.valtimo.contract.authentication.AuthoritiesConstants.USER
 import com.ritense.valtimo.contract.security.config.HttpConfigurerConfigurationException
 import com.ritense.valtimo.contract.security.config.HttpSecurityConfigurer
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
@@ -19,7 +17,7 @@ class SamenwerkfunctionaliteitHttpSecurityConfig : HttpSecurityConfigurer {
             http.authorizeHttpRequests { requests ->
                 requests
                     .requestMatchers(HttpMethod.GET, "samenwerkfunctionaliteit/v5/**")
-                    .hasAnyAuthority(ADMIN, USER)
+                    .permitAll()
             }
         } catch (e: Exception) {
             throw HttpConfigurerConfigurationException(e)
