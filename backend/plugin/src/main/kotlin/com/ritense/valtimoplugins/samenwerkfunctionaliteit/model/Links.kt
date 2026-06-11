bfe6ac2a1c39f9f0a14bedfacee533fcf78d9cae
furkang-risa <Furkan@risa-it.nl>
2026-06-11 08:52:00 +0200

    Remove underscore

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/model/Links.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/model/Links.kt
@@ -1,5 +1,5 @@
 package com.ritense.valtimoplugins.samenwerkfunctionaliteit.model
 
 data class Links(
-    val _additionalProperties: Map<String, Link> = mutableMapOf()
+    val additionalProperties: Map<String, Link> = mutableMapOf()
 )
