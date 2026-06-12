06ff0a10fb3ad9ef3f407cb797e37d8c6f4b972e
furkang-risa <Furkan@risa-it.nl>
2026-06-12 15:28:40 +0200

    Remove status object, should be received from samenwerking

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/samenwerkingsinformatiepagina/swf-informatie-pagina/swf-informatie-pagina.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/samenwerkingsinformatiepagina/swf-informatie-pagina/swf-informatie-pagina.component.ts
@@ -1,6 +1,5 @@
 import {Component, DestroyRef, inject, OnInit, signal, WritableSignal} from "@angular/core";
 import {SamenwerkingsStatusComponent} from "../samenwerkingsstatus/samenwerkingsstatus.component";
-import {Status} from "../model/status.model";
 import {SamenwerkingsinformatiepaginaService} from "../service/samenwerkingsinformatiepagina.service";
 import {Subject, takeUntil} from "rxjs";
 import {Samenwerking} from "../model/samenwerking.model";
@@ -21,10 +20,6 @@ export class SwfInformatiePaginaComponent implements OnInit {
 
   samenwerkingService = inject(SamenwerkingsinformatiepaginaService);
 
-  status: Status = {
-    status: "OPEN"
-  };
-
   samenwerking: WritableSignal<Samenwerking> = signal(null);
   samenwerkingId: string = "SAM-66497";
 
@@ -34,7 +29,7 @@ export class SwfInformatiePaginaComponent implements OnInit {
 
 
   ngOnInit() {
-    this.samenwerkingService.getSamenwerking("SAM-66497").pipe(takeUntil(this.destroy$)).subscribe({
+    this.samenwerkingService.getSamenwerking(this.samenwerkingId).pipe(takeUntil(this.destroy$)).subscribe({
       next: (samenwerking: Samenwerking) => this.samenwerking.set(samenwerking),
       error: (error: Error) => console.log(error.message)
     })
