6ed1727157e40d27483de4ac7b7414fc846c6f31
furkang-risa <Furkan@risa-it.nl>
2026-06-12 15:09:39 +0200

    Update swf component to use samenwerking

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/samenwerkingsinformatiepagina/swf-informatie-pagina/swf-informatie-pagina.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/samenwerkingsinformatiepagina/swf-informatie-pagina/swf-informatie-pagina.component.ts
@@ -1,6 +1,10 @@
-import {Component} from "@angular/core";
+import {Component, DestroyRef, inject, OnInit, signal, WritableSignal} from "@angular/core";
 import {SamenwerkingsStatusComponent} from "../samenwerkingsstatus/samenwerkingsstatus.component";
 import {Status} from "../model/status.model";
+import {SamenwerkingsinformatiepaginaService} from "../service/samenwerkingsinformatiepagina.service";
+import {Subject, takeUntil} from "rxjs";
+import {Samenwerking} from "../model/samenwerking.model";
+import {SamenwerkingComponent} from "../samenwerking/samenwerking.component";
 
 
 @Component({
@@ -8,13 +12,34 @@ import {Status} from "../model/status.model";
   templateUrl: './swf-informatie-pagina.component.html',
   standalone: true,
   imports: [
-    SamenwerkingsStatusComponent
+    SamenwerkingsStatusComponent,
+    SamenwerkingComponent,
   ],
   styleUrl: './swf-informatie-pagina.component.scss'
 })
-export class SwfInformatiePaginaComponent {
+export class SwfInformatiePaginaComponent implements OnInit {
+
+  samenwerkingService = inject(SamenwerkingsinformatiepaginaService);
+
   status: Status = {
     status: "OPEN"
   };
+
+  samenwerking: WritableSignal<Samenwerking> = signal(null);
+  samenwerkingId: string = "SAM-66497";
+
+
+  destroyRef: DestroyRef = inject(DestroyRef);
+  destroy$: Subject<void> = new Subject<void>();
+
+
+  ngOnInit() {
+    this.samenwerkingService.getSamenwerking("SAM-66497").pipe(takeUntil(this.destroy$)).subscribe({
+      next: (samenwerking: Samenwerking) => this.samenwerking.set(samenwerking),
+      error: (error: Error) => console.log(error.message)
+    })
+    this.destroyRef.onDestroy(() => this.destroy$.next());
+  }
+
 
 }
