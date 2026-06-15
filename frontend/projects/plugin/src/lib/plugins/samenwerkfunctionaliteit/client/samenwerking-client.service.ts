c2ffa45fae2f4d3b63383f01c0a09999fe891e2c
furkang-risa <Furkan@risa-it.nl>
2026-06-15 16:30:06 +0200

    Remove baseUrl, is in the DI httpClient

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/samenwerking-client.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/samenwerking-client.service.ts
@@ -8,16 +8,13 @@ import {Observable} from "rxjs";
   providedIn: 'root'
 })
 export class SamenwerkingClient {
-
   private readonly http: HttpClient = inject(HttpClient);
-  private readonly baseUrl: string = "http://localhost:8080";
   private readonly SAMENWERKINGEN_URL = "samenwerkfunctionaliteit/v5/samenwerkingen"
 
   getSamenwerking(samenwerkingId: string): Observable<Samenwerking> {
     const httpHeaders = new HttpHeaders({
       "x-dienst": "ggd-hl"
     })
-
-    return this.http.get<Samenwerking>(`${this.baseUrl}/${this.SAMENWERKINGEN_URL}/${samenwerkingId}`, {headers: httpHeaders})
+    return this.http.get<Samenwerking>(`/${this.SAMENWERKINGEN_URL}/${samenwerkingId}`, {headers: httpHeaders})
   }
 }
