a9da44439404ed23b2dcaa58fd4055d2eacbc686
furkang-risa <Furkan@risa-it.nl>
2026-06-15 13:35:03 +0200

    Add error handling and isLoading to handle null.

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/samenwerkingsinformatiepagina/swf-informatie-pagina/swf-informatie-pagina.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/samenwerkingsinformatiepagina/swf-informatie-pagina/swf-informatie-pagina.component.ts
@@ -1,7 +1,7 @@
 import {Component, DestroyRef, inject, OnInit, signal, WritableSignal} from "@angular/core";
 import {SamenwerkingsStatusComponent} from "../samenwerkingsstatus/samenwerkingsstatus.component";
 import {SamenwerkingsinformatiepaginaService} from "../service/samenwerkingsinformatiepagina.service";
-import {Subject, takeUntil} from "rxjs";
+import {skipWhile, Subject, takeUntil} from "rxjs";
 import {Samenwerking} from "../model/samenwerking.model";
 import {SamenwerkingComponent} from "../samenwerking/samenwerking.component";
 
@@ -17,24 +17,38 @@ import {SamenwerkingComponent} from "../samenwerking/samenwerking.component";
   styleUrl: './swf-informatie-pagina.component.scss'
 })
 export class SwfInformatiePaginaComponent implements OnInit {
-
   samenwerkingService = inject(SamenwerkingsinformatiepaginaService);
 
   samenwerking: WritableSignal<Samenwerking> = signal(null);
-  samenwerkingId: string = "SAM-66497";
+  isLoading: WritableSignal<boolean> = signal(true);
 
+  hasError: WritableSignal<boolean> = signal(false);
+  errorMessage: WritableSignal<string> = signal("");
+
+  samenwerkingId: string = "SAM-66497"; //TODO samenwerkingId komt uit document (scheduler service)
 
   destroyRef: DestroyRef = inject(DestroyRef);
   destroy$: Subject<void> = new Subject<void>();
 
-
   ngOnInit() {
-    this.samenwerkingService.getSamenwerking(this.samenwerkingId).pipe(takeUntil(this.destroy$)).subscribe({
-      next: (samenwerking: Samenwerking) => this.samenwerking.set(samenwerking),
-      error: (error: Error) => console.log(error.message)
-    })
+    this.loadSamenwerking();
     this.destroyRef.onDestroy(() => this.destroy$.next());
   }
 
+  private loadSamenwerking(): void {
+    this.samenwerkingService.getSamenwerking(this.samenwerkingId).pipe(
+      skipWhile(samenwerking => samenwerking === null),
+      takeUntil(this.destroy$)
+    ).subscribe({
+      next: (samenwerking: Samenwerking) => {
+        this.isLoading.set(false);
+        this.samenwerking.update(() => samenwerking)
+      },
+      error: (error: Error) => {
+        this.hasError.set(true);
+        this.errorMessage.set(error.message);
+      }
+    })
+  }
 
 }
