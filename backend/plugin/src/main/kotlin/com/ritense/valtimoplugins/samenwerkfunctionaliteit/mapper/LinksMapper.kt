ad0e6a6b046aebc628b37c41e1b03db15a92cb0a
furkang-risa <Furkan@risa-it.nl>
2026-06-16 11:21:25 +0200

    Fix member name

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/mapper/LinksMapper.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/mapper/LinksMapper.kt
@@ -4,9 +4,9 @@ import com.ritense.valtimoplugins.samenwerkfunctionaliteit.model.Links
 import com.ritense.valtimoplugins.samenwerkfunctionaliteit.dto.Links as LinksDTO
 
 fun Links.toDTO() = LinksDTO(
-    additionalProperties = additionalProperties.mapValues { (_, link) -> link.toDTO() }
+    additionalProperties = _additionalProperties.mapValues { (_, link) -> link.toDTO() }
 )
 
 fun LinksDTO.toModel() = Links(
-    additionalProperties = additionalProperties.mapValues { (_, link) -> link.toModel() }
+    _additionalProperties = additionalProperties.mapValues { (_, link) -> link.toModel() }
 )
