25e1cd8fb4f652a3363af6f774f97ac93285c285
Jasper Kunkeler <jasper@risa-it.nl>
2026-06-17 16:18:53 +0200

    Change back before and add header remover in .after

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/gateway/GatewayConfig.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/gateway/GatewayConfig.kt
@@ -3,13 +3,14 @@ package com.ritense.valtimoplugins.samenwerkfunctionaliteit.gateway
 import com.ritense.plugin.service.PluginConfigurationSearchParameters
 import com.ritense.plugin.service.PluginService
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
 import org.springframework.web.servlet.function.RequestPredicates.path
 import org.springframework.web.servlet.function.RouterFunction
-import org.springframework.web.servlet.function.ServerRequest
 import org.springframework.web.servlet.function.ServerResponse
 import java.net.URI
 
@@ -18,7 +19,6 @@ class GatewayConfig(
     private val gatewayProperties: GatewayProperties,
     private val permissionFilter: PermissionFilter,
     private val headerProcessingFilter: HeaderProcessingFilter,
-    private val responseFilter: ResponseFilter,
     private val pluginService: PluginService,
 ) {
     @Bean
@@ -30,26 +30,26 @@ class GatewayConfig(
     fun samenwerkfunctionaliteitRoute(): RouterFunction<ServerResponse> =
         route()
             .route(path(gatewayProperties.endpoint), http())
-            .before { request ->
-                val targetUri = getApiUrl()
-                ServerRequest.from(request).uri(targetUri).build()
-            }.filter(permissionFilter)
+            .before(uri(getApiUrl()))
+            .filter(permissionFilter)
             .filter(headerProcessingFilter)
-            .filter(responseFilter)
-            .build()
+            .after { _, response ->
+                response.headers().remove(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN)
+                response
+            }.build()
 
     private fun getApiUrl(): URI {
         val urlAsString =
             gatewayProperties.baseUrl ?: getApiUrlFromPluginService()
 
         if (urlAsString.isNullOrBlank()) {
-            throw IllegalStateException("Missing or invalid base API URL configuration for Samenwerkfunctionaliteit")
+            throw IllegalArgumentException("Missing or invalid base API URL configuration for Samenwerkfunctionaliteit")
         }
 
         return URI.create(urlAsString)
     }
 
-    fun getApiUrlFromPluginService(): String? =
+    private fun getApiUrlFromPluginService(): String? =
         pluginService
             .getPluginConfigurations(
                 PluginConfigurationSearchParameters(
