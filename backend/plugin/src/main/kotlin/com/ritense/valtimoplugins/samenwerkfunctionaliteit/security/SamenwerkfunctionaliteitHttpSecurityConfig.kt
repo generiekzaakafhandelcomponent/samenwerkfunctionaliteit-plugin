0d769a756bd13b9344e10d62f40ecd213d36043e
Jasper Kunkeler <jasper@risa-it.nl>
2026-06-17 16:11:07 +0200

    Use gatewayProperties in security

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/security/SamenwerkfunctionaliteitHttpSecurityConfig.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/security/SamenwerkfunctionaliteitHttpSecurityConfig.kt
@@ -4,6 +4,7 @@ import com.ritense.valtimo.contract.authentication.AuthoritiesConstants.ADMIN
 import com.ritense.valtimo.contract.authentication.AuthoritiesConstants.USER
 import com.ritense.valtimo.contract.security.config.HttpConfigurerConfigurationException
 import com.ritense.valtimo.contract.security.config.HttpSecurityConfigurer
+import com.ritense.valtimoplugins.samenwerkfunctionaliteit.gateway.GatewayProperties
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.core.annotation.Order
 import org.springframework.http.HttpMethod
@@ -13,12 +14,14 @@ import org.springframework.stereotype.Component
 @Order(1)
 @Component
 @ConditionalOnProperty(prefix = "valtimo.samenwerkfunctionaliteit.gateway", name = ["enabled"], havingValue = "true")
-class SamenwerkfunctionaliteitHttpSecurityConfig : HttpSecurityConfigurer {
+class SamenwerkfunctionaliteitHttpSecurityConfig(
+    var gatewayProperties: GatewayProperties,
+) : HttpSecurityConfigurer {
     override fun configure(http: HttpSecurity) {
         try {
             http.authorizeHttpRequests { requests ->
                 requests
-                    .requestMatchers(HttpMethod.GET, "samenwerkfunctionaliteit/v5/**")
+                    .requestMatchers(HttpMethod.GET, gatewayProperties.endpoint)
                     .hasAnyAuthority(ADMIN, USER)
             }
         } catch (e: Exception) {
