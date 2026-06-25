e1488eb1ddd8222efca402b5fe920fcb68f97dde
Jasper Kunkeler <jasper@risa-it.nl>
2026-06-25 13:54:37 +0200

    Remove rewritePath in gateway

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/gateway/GatewayProperties.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/gateway/GatewayProperties.kt
@@ -7,7 +7,5 @@ import org.springframework.stereotype.Component
 @ConfigurationProperties(prefix = "valtimo.samenwerkfunctionaliteit.gateway")
 class GatewayProperties {
     var customHeaders: Map<String, String> = emptyMap()
-    var gatewayEndpoint: String = "/samenwerkfunctionaliteit/v1"
     var baseUrl: String? = null
-    var apiEndpoint: String? = null
 }
