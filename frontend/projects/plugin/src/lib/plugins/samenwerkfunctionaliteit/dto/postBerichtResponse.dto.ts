c591583169a61fc11f22a531b52bd0f553f1f6e4
Gustaaf-RISA <gustaaf@risa-it.nl>
2026-06-29 16:32:44 +0200

    Add bericht request and response DTOs

---
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/dto/postBerichtResponse.dto.ts
@@ -0,0 +1,14 @@
+import {Links} from "./links.dto";
+
+export interface PostBerichtResponse {
+  _links?: Links;
+  actieverzoekId: string;
+  berichtId: string;
+  creatieDatumTijd: string;
+  inhoud: string;
+  ontvanger: string;
+  ontvangerNaam?: string;
+  samenwerkingId?: string;
+  zender?: string;
+  zenderNaam?: string;
+}
