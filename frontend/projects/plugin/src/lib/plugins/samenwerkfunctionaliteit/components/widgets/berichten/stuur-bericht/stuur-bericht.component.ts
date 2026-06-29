2df216319c62f5bde21d29aaaa2eeebe094e79ad
Gustaaf-RISA <gustaaf@risa-it.nl>
2026-06-29 16:34:09 +0200

    Read Document ActieverzoekId in widget

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/widgets/berichten/stuur-bericht/stuur-bericht.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/widgets/berichten/stuur-bericht/stuur-bericht.component.ts
@@ -1,35 +1,52 @@
-import {Component} from '@angular/core';
-import { InputModule, ButtonModule, IconModule, IconService } from 'carbon-components-angular';
-import {FormsModule} from '@angular/forms';
-import {Send32} from '@carbon/icons';
+import { Component } from "@angular/core";
+import { InputModule, ButtonModule, IconModule, IconService } from "carbon-components-angular";
+import { FormsModule } from "@angular/forms";
+import { Send32 } from "@carbon/icons";
+import { ActivatedRoute } from "@angular/router";
+import { NGXLogger } from "ngx-logger";
+import { Document as ValtimoDocument, DocumentService } from "@valtimo/document";
+import { take } from "rxjs";
 
-import {ProcessInstance, ProcessService} from '@valtimo/process';
-import {ActivatedRoute} from '@angular/router';
-import {NGXLogger} from 'ngx-logger';
+type SwfContent = {
+  samenwerkfunctionaliteit?: { actieverzoekId: string  };
+};
 
 @Component({
-    selector: 'stuur-bericht',
-    imports: [InputModule, ButtonModule, IconModule, FormsModule],
-    templateUrl: './stuur-bericht.component.html',
-    styleUrl: './stuur-bericht.component.scss'
+  selector: "stuur-bericht",
+  imports: [InputModule, ButtonModule, IconModule, FormsModule],
+  templateUrl: "./stuur-bericht.component.html",
+  styleUrl: "./stuur-bericht.component.scss",
 })
 export class StuurBerichtComponent {
-    placeholder = 'Type hier uw bericht';
-    rows = 5;
-    enableCounter = true;
-    maxLength = 512;
-    message = "";
+  private _documentId: string | undefined;
+  private _document: ValtimoDocument | undefined;
+  private actieverzoekId: string | null;
+  placeholder = "Type hier uw bericht";
+  rows = 5;
+  enableCounter = true;
+  maxLength = 512;
+  message = "";
 
-    constructor(
-      private route: ActivatedRoute,
-      private processService: ProcessService,
-      private readonly logger: NGXLogger,
-      private readonly iconService: IconService
-    ) {
-        this.iconService.registerAll([Send32]);
-    }
+  constructor(
+    private route: ActivatedRoute,
+    private documentService: DocumentService,
+    private readonly logger: NGXLogger,
+    private readonly iconService: IconService,
+  ) {
+    this.iconService.registerAll([Send32]);
+  }
 
-    onClick() {
-        this.logger.info(this.message);
-    }
+  ngOnInit() {
+    this._documentId = this.route.snapshot.paramMap.get('documentId') || '';
+    this.documentService.getDocument(this._documentId).pipe(take(1)).subscribe(doc => {
+      this._document = doc;
+      const documentContent = this._document.content as Partial<SwfContent>
+      this.actieverzoekId = documentContent?.samenwerkfunctionaliteit?.actieverzoekId ?? null;
+    });
+  }
+
+  onClick() {
+    this.logger.info(this.actieverzoekId);
+    this.logger.info(this.message);
+  }
 }
