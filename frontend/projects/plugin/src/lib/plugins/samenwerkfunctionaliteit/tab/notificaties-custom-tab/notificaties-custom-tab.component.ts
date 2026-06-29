1ec2fc3db1835048404e9ccc50d074b68250dc20
furkang-risa <Furkan@risa-it.nl>
2026-06-29 12:58:58 +0200

    (Test) swf-notificatie in tab

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/tab/notificaties-custom-tab/notificaties-custom-tab.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/tab/notificaties-custom-tab/notificaties-custom-tab.component.ts
@@ -1,11 +1,27 @@
-/*
- * Created by mika on 2026-06-03
-*/
-
 import {Component} from '@angular/core';
+import {NotificatieComponent} from "../../components/notificaties-lijst/notificatie/notificatie.component";
+import {NotificatieCardInput, NotificatieType} from "../../components/notificaties-lijst/interface/notificatie-card.interface";
 
 @Component({
-  template: ``,
+  templateUrl: `notificaties-custom-tab.component.html`,
+  imports: [NotificatieComponent],
   selector: "notificaties-custom-tab",
 })
-export class NotificatiesCustomTabComponent {}
+export class NotificatiesCustomTabComponent {
+
+  inputs: NotificatieCardInput = {
+    notificatieId: "eb53b7c5-ea64-4c49-94db-67aa7cac8e05",
+    type: NotificatieType.BERICHT,
+    colorCode: "BLUE",
+    title: "Actieverzoek ontvangen",
+    eventDateTime: new Date(),
+    initiatorNaam: "Omgevingsdienst Rommelerwaard",
+    content: "Omgevingsdienst Rommelerwaard heeft u het\n" +
+      "actieverzoek \"Adviesaanvraag bouwwerkzaamheden Berkendaal\"\n" +
+      "gestuurd",
+  }
+
+  ngOnInit(){
+
+  }
+}
