70182307f5bd66f9b13815909fa90569e141b78d
Gustaaf-RISA <gustaaf@risa-it.nl>
2026-07-09 15:52:32 +0200

    Add response typing to berichten service

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/service/berichten.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/service/berichten.service.ts
@@ -1,12 +1,14 @@
 import { inject, Injectable } from "@angular/core";
 import { BerichtenClient } from "../client/berichten-client.service";
 import { PostBerichtRequest } from "../dto/postBerichtRequest.dto";
+import { Observable } from "rxjs";
+import { PostBerichtResponse } from "../dto/postBerichtResponse.dto";
 
 @Injectable({ providedIn: "root", })
 export class BerichtenService {
   berichtenClient = inject(BerichtenClient);
 
-  postBericht(actieverzoekId: string, bericht: string) {
+  postBericht(actieverzoekId: string, bericht: string): Observable<PostBerichtResponse> {
     const berichtBody: PostBerichtRequest = {
       bericht: bericht.trim()
     }
