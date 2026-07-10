7e899f3552e9e1443d969243f6f034552f2d7453
furkang-risa <Furkan@risa-it.nl>
2026-07-10 11:50:46 +0200

    Use swfdocumentservice to get documentid and samenwerkingid from content

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/tab/notificaties-custom-tab/notificaties-custom-tab.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/tab/notificaties-custom-tab/notificaties-custom-tab.component.ts
@@ -2,10 +2,13 @@ import {Component, inject, signal, WritableSignal} from '@angular/core';
 import {NotificatieComponent} from "../../components/notificaties-lijst/notificatie/notificatie.component";
 import {NotificatieCardInput, NotificatieCardType} from "../../components/notificaties-lijst/interface/notificatie-card.interface";
 import {NotificatieService} from "../../service/notificatie.service";
-import {take} from "rxjs";
+import {Observable, switchMap, take} from "rxjs";
 import {Notificatie} from "../../models/notificatie.model";
 import {getNotificationCardTypeByNotificationType} from "../../components/notificaties-lijst/config/notificatie-type-config";
 import {LoadingModule, SkeletonModule} from "carbon-components-angular";
+import {SwfDocumentService} from "../../service/swf-document.service";
+import {ActivatedRoute} from "@angular/router";
+import {BusinessKey} from "../../models/business-key.model";
 
 @Component({
   templateUrl: `notificaties-custom-tab.component.html`,
@@ -15,6 +18,8 @@ import {LoadingModule, SkeletonModule} from "carbon-components-angular";
 })
 export class NotificatiesCustomTabComponent {
   notificatieService: NotificatieService = inject(NotificatieService);
+  swfDocumentService: SwfDocumentService = inject(SwfDocumentService);
+  route: ActivatedRoute = inject(ActivatedRoute);
   notifications: WritableSignal<Notificatie[]> = signal<Notificatie[]>([]);
   inputs: WritableSignal<NotificatieCardInput[]> = signal<NotificatieCardInput[]>([]);
   isLoading: WritableSignal<boolean> = signal(true);
@@ -22,19 +27,32 @@ export class NotificatiesCustomTabComponent {
   itemsPerPageArray: number[] = Array.from({length: this.itemsPerPage});
 
   ngOnInit() {
-    this.loadNotifications();
+    const documentId = this.swfDocumentService.getParam(this.route, 'documentId');
+    this.fetchAndLoadNotifications(documentId);
   }
 
-  loadNotifications() {
-    this.notificatieService.getNotificaties("SAM-66497")
-      .pipe(take(1))
-      .subscribe(notificaties => {
-        this.notifications.set(notificaties)
-        this.loadInputs(this.notifications());
+  private fetchAndLoadNotifications(documentId: string): void {
+    const valtimoBusinessKey: BusinessKey = {
+      value: documentId,
+    }
+
+    this.swfDocumentService.getSamenwerkingProperties(valtimoBusinessKey).pipe(
+      take(1),
+      switchMap((samenwerkingProperties) => {
+        return this.fetchNotifications(samenwerkingProperties.samenwerkingId)
       })
+    ).subscribe(notificaties => {
+      this.notifications.set(notificaties)
+      this.loadInputs(this.notifications());
+    })
   }
 
-  loadInputs(notificaties: Notificatie[]) {
+  private fetchNotifications(samenwerkingId: string): Observable<Notificatie[]> {
+    return this.notificatieService.getNotificaties(samenwerkingId)
+      .pipe(take(1))
+  }
+
+  private loadInputs(notificaties: Notificatie[]): void {
     this.inputs.set(notificaties.map((notificatie) => {
       return this.mapNotificatieToNotificatieCardInput(notificatie)
     }))
