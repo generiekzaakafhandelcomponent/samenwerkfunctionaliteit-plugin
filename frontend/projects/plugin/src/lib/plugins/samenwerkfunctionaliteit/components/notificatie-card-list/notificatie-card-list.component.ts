00bc87a5065975389b26dc2fcb709a049be53154
Axel Plowman <axel@risa-it.nl>
2026-07-13 14:16:14 +0200

    Formatting

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/notificatie-card-list/notificatie-card-list.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/notificatie-card-list/notificatie-card-list.component.ts
@@ -1,14 +1,14 @@
-import {Component, inject, signal, WritableSignal} from '@angular/core';
-import {NotificatieComponent} from "../../components/notificaties-lijst/notificatie/notificatie.component";
-import {NotificatieCardInput} from "../../components/notificaties-lijst/interface/notificatie-card.interface";
-import {NotificatieService} from "../../service/notificatie.service";
-import {Observable, switchMap, take} from "rxjs";
-import {Notificatie} from "../../models/notificatie.model";
-import {getNotificationCardTypeByNotificationType} from "../../components/notificaties-lijst/config/notificatie-type-config";
-import {LoadingModule, SkeletonModule} from "carbon-components-angular";
-import {SwfDocumentService} from "../../service/swf-document.service";
-import {ActivatedRoute} from "@angular/router";
-import {BusinessKey} from "../../models/business-key.model";
+import { Component, inject, signal, WritableSignal } from "@angular/core";
+import { NotificatieComponent } from "../../components/notificaties-lijst/notificatie/notificatie.component";
+import { NotificatieCardInput } from "../../components/notificaties-lijst/interface/notificatie-card.interface";
+import { NotificatieService } from "../../service/notificatie.service";
+import { Observable, switchMap, take } from "rxjs";
+import { Notificatie } from "../../models/notificatie.model";
+import { getNotificationCardTypeByNotificationType } from "../../components/notificaties-lijst/config/notificatie-type-config";
+import { LoadingModule, SkeletonModule } from "carbon-components-angular";
+import { SwfDocumentService } from "../../service/swf-document.service";
+import { ActivatedRoute } from "@angular/router";
+import { BusinessKey } from "../../models/business-key.model";
 
 @Component({
   templateUrl: `notificatie-card-list.component.html`,
@@ -24,45 +24,47 @@ export class NotificatieCardList {
   inputs: WritableSignal<NotificatieCardInput[]> = signal<NotificatieCardInput[]>([]);
   isLoading: WritableSignal<boolean> = signal(true);
   itemsPerPage = 10;
-  itemsPerPageArray: number[] = Array.from({length: this.itemsPerPage});
+  itemsPerPageArray: number[] = Array.from({ length: this.itemsPerPage });
 
   ngOnInit() {
-    const documentId = this.swfDocumentService.getParam(this.route, 'documentId');
+    const documentId = this.swfDocumentService.getParam(this.route, "documentId");
     this.fetchAndLoadNotifications(documentId);
   }
 
   private fetchAndLoadNotifications(documentId: string): void {
     const valtimoBusinessKey: BusinessKey = {
       value: documentId,
-    }
+    };
 
-    this.swfDocumentService.getSamenwerkingProperties(valtimoBusinessKey).pipe(
-      take(1),
-      switchMap((samenwerkingProperties) => {
-        return this.fetchNotifications(samenwerkingProperties.samenwerkingId)
-      })
-    ).subscribe(notificaties => {
-      this.notifications.set(notificaties)
-      this.loadInputs(this.notifications());
-    })
+    this.swfDocumentService
+      .getSamenwerkingProperties(valtimoBusinessKey)
+      .pipe(
+        take(1),
+        switchMap((samenwerkingProperties) => {
+          return this.fetchNotifications(samenwerkingProperties.samenwerkingId);
+        }),
+      )
+      .subscribe((notificaties) => {
+        this.notifications.set(notificaties);
+        this.loadInputs(this.notifications());
+      });
   }
 
   private fetchNotifications(samenwerkingId: string): Observable<Notificatie[]> {
-    return this.notificatieService.getNotificaties(samenwerkingId)
-      .pipe(take(1))
+    return this.notificatieService.getNotificaties(samenwerkingId).pipe(take(1));
   }
 
   private loadInputs(notificaties: Notificatie[]): void {
-    this.inputs.set(notificaties.map((notificatie) => {
-      return this.mapNotificatieToNotificatieCardInput(notificatie)
-    }))
+    this.inputs.set(
+      notificaties.map((notificatie) => {
+        return this.mapNotificatieToNotificatieCardInput(notificatie);
+      }),
+    );
     this.isLoading.set(false);
   }
 
-  private mapNotificatieToNotificatieCardInput(
-    notificatie: Notificatie
-  ): NotificatieCardInput {
-    const type = getNotificationCardTypeByNotificationType(notificatie.notificatieType)
+  private mapNotificatieToNotificatieCardInput(notificatie: Notificatie): NotificatieCardInput {
+    const type = getNotificationCardTypeByNotificationType(notificatie.notificatieType);
 
     const eventDateTime = new Date(notificatie.eventDatumTijd);
 
