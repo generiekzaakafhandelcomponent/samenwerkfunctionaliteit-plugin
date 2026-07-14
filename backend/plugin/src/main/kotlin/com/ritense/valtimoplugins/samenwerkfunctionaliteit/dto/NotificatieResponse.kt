587226de9ca2c60a55efd6ae1d0b8f2561faeb94
furkang-risa <Furkan@risa-it.nl>
2026-07-14 15:40:32 +0200

    Add toModel and remove duplicate @fields

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/dto/NotificatieResponse.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/dto/NotificatieResponse.kt
@@ -1,13 +1,14 @@
 package com.ritense.valtimoplugins.samenwerkfunctionaliteit.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.ritense.valtimoplugins.samenwerkfunctionaliteit.model.Links
+import com.ritense.valtimoplugins.samenwerkfunctionaliteit.mapper.toModel
+import com.ritense.valtimoplugins.samenwerkfunctionaliteit.model.Notificatie
 
 data class NotificatieResponse(
-    @get:JsonProperty("notificatieId")@field:JsonProperty("notificatieId")
+    @get:JsonProperty("notificatieId")
     val notificatieID: String? = null,
     val notificatieType: String? = null,
-    @get:JsonProperty("samenwerkingId")@field:JsonProperty("samenwerkingId")
+    @get:JsonProperty("samenwerkingId")
     val samenwerkingID: String? = null,
     val samenwerkVorm: String? = null,
     val notificatieTitel: String? = null,
@@ -18,6 +19,24 @@ data class NotificatieResponse(
     val deelnemerNaam: String? = null,
     val eventDatumTijd: String? = null,
     val properties: Map<String, String?>? = null,
-    @get:JsonProperty("_links")@field:JsonProperty("_links")
+    @get:JsonProperty("_links")
     val links: Links? = null,
 )
+
+fun NotificatieResponse.toModel(): Notificatie {
+    return Notificatie(
+        notificatieID = notificatieID,
+        notificatieType = notificatieType,
+        samenwerkingID = samenwerkingID,
+        samenwerkVorm = samenwerkVorm,
+        notificatieTitel = notificatieTitel,
+        notificatieTekst = notificatieTekst,
+        eventInitiator = eventInitiator,
+        eventInitiatorNaam = eventInitiatorNaam,
+        deelnemer = deelnemer,
+        deelnemerNaam = deelnemerNaam,
+        eventDatumTijd = eventDatumTijd,
+        properties = properties,
+        links = links?.toModel()
+    )
+}
