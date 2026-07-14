1a88fd313e2501710d75a917758a1df3a5ff6706
furkang-risa <Furkan@risa-it.nl>
2026-07-14 15:42:22 +0200

    Fix property name, was changed in previous commit

--- backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/mapper/LinksMapper.kt
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/mapper/LinksMapper.kt
@@ -4,9 +4,9 @@ import com.ritense.valtimoplugins.samenwerkfunctionaliteit.model.Links
 import com.ritense.valtimoplugins.samenwerkfunctionaliteit.dto.Links as LinksDTO
 
 fun Links.toDTO() = LinksDTO(
-    additionalProperties = _additionalProperties.mapValues { (_, link) -> link.toDTO() }
+    additionalProperties = additionalProperties.mapValues { (_, link) -> link.toDTO() }
 )
 
 fun LinksDTO.toModel() = Links(
-    _additionalProperties = additionalProperties.mapValues { (_, link) -> link.toModel() }
+    additionalProperties = additionalProperties.mapValues { (_, link) -> link.toModel() }
 )
