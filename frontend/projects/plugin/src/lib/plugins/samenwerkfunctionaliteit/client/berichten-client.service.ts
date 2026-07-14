95312c8970a8c798a1fe51cc5ae01439f114e3e7
Gustaaf-RISA <gustaaf@risa-it.nl>
2026-07-14 11:13:26 +0200

    REFACTOR  Actieverzoeken URL for berichten

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/berichten-client.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/berichten-client.service.ts
@@ -3,7 +3,7 @@ import { inject, Injectable } from "@angular/core";
 import { PostBerichtRequestDto } from "../dto/post-bericht-request.dto";
 import { PostBerichtResponseDto } from "../dto/post-bericht-response.dto";
 import { Observable } from "rxjs";
-import { SAMENWERKINGEN_URL } from "../config/samenwerking-config";
+import { ACTIEVERZOEKEN_URL } from "../config/samenwerking-config";
 
 
 @Injectable({
@@ -13,6 +13,6 @@ export class BerichtenClient {
   private readonly http: HttpClient = inject(HttpClient);
 
   postBericht(actieverzoekId: string, bericht: PostBerichtRequestDto): Observable<PostBerichtResponseDto> {
-    return this.http.post<PostBerichtResponseDto>(`/${SAMENWERKINGEN_URL}/actieverzoeken/${actieverzoekId}/berichten`, bericht)
+    return this.http.post<PostBerichtResponseDto>(`/${ACTIEVERZOEKEN_URL}/${actieverzoekId}/berichten`, bericht)
   }
 }
--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/config/samenwerking-config.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/config/samenwerking-config.ts
@@ -1,1 +1,1 @@
-export const SAMENWERKINGEN_URL = "samenwerkfunctionaliteit/v5/samenwerkingen";
+export const ACTIEVERZOEKEN_URL = "samenwerkfunctionaliteit/v5/actieverzoeken";
