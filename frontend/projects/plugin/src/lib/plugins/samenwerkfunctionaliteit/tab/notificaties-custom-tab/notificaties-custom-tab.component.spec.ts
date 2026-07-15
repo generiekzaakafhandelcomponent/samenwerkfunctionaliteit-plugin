f06349da79d4a3d0a2232f9db46029aa6db2728a
Matthijs Boonstra <matthijs.boonstra@risa-it.nl>
2026-07-15 11:27:56 +0200

    Fixed tests not being runnable
    - No karma.conf.js was available to run the plugin frontend tests. This was added.
    - Changed angular.json - "main" is an outdated option.
    - Fixed specs throwing NullImports - added imports & fixed dependency injection
    - Removed "files" from tsconfig.spec.json - Karma auto-detects the right files.

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/tab/notificaties-custom-tab/notificaties-custom-tab.component.spec.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/tab/notificaties-custom-tab/notificaties-custom-tab.component.spec.ts
@@ -1,5 +1,9 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ActivatedRoute, convertToParamMap } from "@angular/router";
+import { of } from "rxjs";
 import { NotificatiesCustomTab } from "./notificaties-custom-tab.component";
+import { NotificatieService } from "../../service/notificatie.service";
+import { SwfDocumentService } from "../../service/swf-document.service";
 
 describe("NotificatiesCustomTabComponent", () => {
   let component: NotificatiesCustomTab;
@@ -8,6 +12,20 @@ describe("NotificatiesCustomTabComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NotificatiesCustomTab],
+      providers: [
+        { provide: NotificatieService, useValue: { getNotificaties: () => of([]) } },
+        {
+          provide: SwfDocumentService,
+          useValue: {
+            getParam: () => null,
+            getSamenwerkingProperties: () => of({ samenwerkingId: "test" }),
+          },
+        },
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { paramMap: convertToParamMap({}) } },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NotificatiesCustomTab);
