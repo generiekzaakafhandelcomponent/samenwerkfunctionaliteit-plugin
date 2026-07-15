73ab737cb79d24da6199988f1ec9096dfb620650
furkang-risa <Furkan@risa-it.nl>
2026-07-15 16:11:11 +0200

    Update client

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/document-client.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/document-client.service.ts
@@ -1,15 +1,18 @@
 import { inject, Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { DocumentenOverzichtResponse } from "../dto/document.dto";
 
 @Injectable({
   providedIn: "root",
 })
 export class DocumentClient {
   private readonly http: HttpClient = inject(HttpClient);
-  private readonly DOCUMENTEN_URL = "samenwerkingen/{{samenwerkingId}}/documenten";
+  private readonly DOCUMENTEN_URL = "documenten";
 
-  getDocumenten(): Observable<DocumentenResponse> {
-    return this.http.get<DocumentenResponse>(`/${this.DOCUMENTEN_URL}`);
+  getDocumenten(samenwerkingId: string): Observable<DocumentenOverzichtResponse> {
+    return this.http.get<DocumentenOverzichtResponse>(
+      `samenwerkfunctionaliteit/v5/samenwerkingen/${samenwerkingId}/${this.DOCUMENTEN_URL}`,
+    );
   }
 }
