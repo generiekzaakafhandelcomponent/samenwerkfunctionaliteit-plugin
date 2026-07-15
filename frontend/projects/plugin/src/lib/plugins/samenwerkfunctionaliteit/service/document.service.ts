1f80aacee7072f41a3b3bf86a55c7ede4ed215b9
furkang-risa <Furkan@risa-it.nl>
2026-07-15 16:10:41 +0200

    Update service

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/service/document.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/service/document.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
+import { DocumentClient } from "../client/document-client.service";
+import { map, Observable } from "rxjs";
+import { DocumentInterface } from "../interface/document.interface";
+import { Documenten, DocumentenOverzichtResponse, mapDocumentenResponseToModels } from "../dto/document.dto";
 
 @Injectable({
   providedIn: "root",
 })
 export class DocumentService {
-  constructor() {}
+  private readonly documentClient: DocumentClient = inject(DocumentClient);
+
+  getDocumenten(samenwerkingId: string): Observable<DocumentInterface[]> {
+    return this.documentClient.getDocumenten(samenwerkingId).pipe(
+      map((documentenOverzichtResponse: DocumentenOverzichtResponse) => {
+        return documentenOverzichtResponse._embedded;
+      }),
+      map((documenten: Documenten) => {
+        return mapDocumentenResponseToModels(documenten);
+      }),
+      map((documenten: DocumentInterface[]) => {
+        return documenten.filter((document) => {
+          return document.samenwerkingId === samenwerkingId;
+        });
+      }),
+    );
+  }
 }
