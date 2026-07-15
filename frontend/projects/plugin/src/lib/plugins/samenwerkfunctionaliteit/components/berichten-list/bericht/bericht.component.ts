e72414a2161d7c1cdf45a269ae8c074e53d0b074
furkang-risa <Furkan@risa-it.nl>
2026-07-15 12:51:49 +0200

    Voeg bericht component toe

---
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/berichten-list/bericht/bericht.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-bericht",
+  imports: [],
+  templateUrl: "./bericht.component.html",
+  styleUrl: "./bericht.component.scss",
+})
+export class BerichtComponent {}
