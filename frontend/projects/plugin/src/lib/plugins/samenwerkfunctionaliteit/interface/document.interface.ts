31e5734ea4b673593ec7deaf17c555e429e8223d
furkang-risa <Furkan@risa-it.nl>
2026-07-16 15:58:13 +0200

    Update document interface

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/interface/document.interface.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/interface/document.interface.ts
@@ -1,7 +1,9 @@
+import { ConfidentialityType } from "../type/confidentiality.type";
+
 export interface DocumentInterface {
   samenwerkingId: string;
   documentId: string;
   fileName: string;
-  confidentialityLevel: string;
+  confidentialityLevel: ConfidentialityType;
   creationDate: string;
 }
