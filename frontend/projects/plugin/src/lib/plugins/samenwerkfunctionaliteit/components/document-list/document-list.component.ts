758db95beab035e95b57ad689d47fc79bae9d061
furkang-risa <Furkan@risa-it.nl>
2026-07-16 15:58:33 +0200

    Voeg table toe met component logica

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/document-list/document-list.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/document-list/document-list.component.ts
@@ -1,9 +1,49 @@
-import { Component } from "@angular/core";
+import { Component, input, InputSignal, OnInit, signal, WritableSignal } from "@angular/core";
+import { ButtonModule, IconModule, PaginationModule, TableHeaderItem, TableItem, TableModel, TableModule } from "carbon-components-angular";
+import { ReactiveFormsModule } from "@angular/forms";
+import { Document } from "../../models/document.model";
+import { NgIf } from "@angular/common";
 
 @Component({
   selector: "document-list",
-  imports: [],
+  imports: [TableModule, ReactiveFormsModule, PaginationModule, NgIf, ButtonModule, IconModule],
   templateUrl: "./document-list.component.html",
   styleUrl: "./document-list.component.css",
 })
-export class DocumentListComponent {}
+export class DocumentListComponent implements OnInit {
+  documents: InputSignal<Document[]> = input<Document[]>([]);
+  skeleton: InputSignal<boolean> = input<boolean>(true);
+  public model: WritableSignal<TableModel> = signal(new TableModel());
+
+  ngOnInit() {
+    console.log(this.documents());
+    this.setModelDataAndHeader(this.documents());
+  }
+
+  private setModelDataAndHeader(documents: Document[]) {
+    this.model().data = this.createTableItemsForTableModel(documents);
+    this.model().header = this.createTableHeadersForTableModel();
+  }
+
+  private createTableItemsForTableModel(documents: Document[]): TableItem[][] {
+    return documents.map((document: Document): TableItem[] => [
+      new TableItem({ data: document.fileName }),
+      new TableItem({ data: document.confidentialityLevel }),
+      new TableItem({ data: document.creationDate }),
+    ]);
+  }
+  //TODO translations
+  private createTableHeadersForTableModel(): TableHeaderItem[] {
+    return [
+      new TableHeaderItem({
+        data: "Bestandsnaam",
+      }),
+      new TableHeaderItem({
+        data: "Vertrouwelijkheidsaanduiding",
+      }),
+      new TableHeaderItem({
+        data: "Datum aangemaakt",
+      }),
+    ];
+  }
+}
