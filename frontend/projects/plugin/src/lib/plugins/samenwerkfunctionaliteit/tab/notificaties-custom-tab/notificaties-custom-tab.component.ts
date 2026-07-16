9fe455188db9e88ccc2e902be64cbf952e20c0d7
Axel Plowman <axel@risa-it.nl>
2026-07-16 21:37:07 +0200

    Fix component selector prefix warning

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/tab/notificaties-custom-tab/notificaties-custom-tab.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/tab/notificaties-custom-tab/notificaties-custom-tab.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 import { NotificatieCardList } from "../../components/notificatie-card-list/notificatie-card-list.component";
 
 @Component({
-  selector: "notificaties-custom-tab",
+  selector: "swf-notificaties-custom-tab",
   imports: [NotificatieCardList],
   templateUrl: "./notificaties-custom-tab.component.html",
   styleUrl: "./notificaties-custom-tab.component.scss",
