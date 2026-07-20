927d1525d8e039bb8abb45261bcf094eae657093
furkang-risa <Furkan@risa-it.nl>
2026-07-20 10:55:11 +0200

    Wrong tab

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/tab/notificaties-custom-tab/notificaties-custom-tab.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/tab/notificaties-custom-tab/notificaties-custom-tab.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { NotificatieCardListComponent } from '../../components/notificatie-card-list/notificatie-card-list.component';
-import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'swf-notificaties-custom-tab',
-  imports: [NotificatieCardListComponent, NgTemplateOutlet],
+  imports: [NotificatieCardListComponent],
   templateUrl: './notificaties-custom-tab.component.html',
   styleUrl: './notificaties-custom-tab.component.scss',
   standalone: true,
