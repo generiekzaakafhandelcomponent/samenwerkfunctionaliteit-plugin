34e48512611577b0edf4b82894d548c4d331e6c3
furkang-risa <Furkan@risa-it.nl>
2026-07-20 15:44:21 +0200

    Remove new line

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/document-list/document-table/light/document-table-light.component.spec.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/document-list/document-table/light/document-table-light.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DocumentTableLightComponent } from './document-table-light.component';
 
 describe('DocumentTableLightComponent', () => {
