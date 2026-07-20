1932521eee026029a379eaaef9abe53f9ace1d7b
furkang-risa <Furkan@risa-it.nl>
2026-07-20 10:46:03 +0200

    Add ngtemplate and document text to the tab

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/tab/notificaties-custom-tab/notificaties-custom-tab.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/tab/notificaties-custom-tab/notificaties-custom-tab.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NotificatieCardListComponent } from '../../components/notificatie-card-list/notificatie-card-list.component';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'swf-notificaties-custom-tab',
-  imports: [NotificatieCardListComponent],
+  imports: [NotificatieCardListComponent, NgTemplateOutlet],
   templateUrl: './notificaties-custom-tab.component.html',
   styleUrl: './notificaties-custom-tab.component.scss',
   standalone: true,
