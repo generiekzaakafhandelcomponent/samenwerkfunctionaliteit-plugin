ba454d02bb9a5d1c493e51aa51111da6c1a8633a
furkang-risa <Furkan@risa-it.nl>
2026-07-20 10:46:15 +0200

    Add openzaak url service

---
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/service/open-zaak-url.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { forkJoin, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { _ } from 'lodash';
+import { OpenZaakInfo } from '../interface/open-zaak-info.interface';
+import { DocumentService as ValtimoDocumentService } from '@valtimo/document';
+import { OpenZaakService } from '@valtimo/resource';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OpenZaakLinkService {
+  private static readonly OPEN_ZAAK_ID_PATH = 'content.openzaak.identificatie';
+
+  constructor(
+    private readonly valtimoDocumentService: ValtimoDocumentService,
+    private readonly openZaakService: OpenZaakService,
+  ) {}
+
+  getOpenZaakInfo(documentId: string): Observable<OpenZaakInfo | null> {
+    return forkJoin({
+      document: this.valtimoDocumentService.getDocument(documentId),
+      zaakTypes: this.openZaakService.getZaakTypes(),
+    }).pipe(
+      map(({ document, zaakTypes }) => {
+        const openZaakId = _.get(
+          document,
+          OpenZaakLinkService.OPEN_ZAAK_ID_PATH,
+        );
+
+        if (!openZaakId) {
+          console.warn(
+            `OpenZaak ID is not available in the document (searching at '${OpenZaakLinkService.OPEN_ZAAK_ID_PATH}').`,
+          );
+          return null;
+        }
+
+        const zaakTypeUrl = zaakTypes[0]?.url;
+
+        if (!zaakTypeUrl) {
+          console.warn(
+            'No zaak types found to retrieve the OpenZaak URL from.',
+          );
+          return null;
+        }
+
+        const baseUrl = new URL(zaakTypeUrl).origin;
+
+        return {
+          id: openZaakId,
+          baseUrl,
+          searchUrl: `${baseUrl}/admin/zaken/zaak/?q=${openZaakId}`,
+        };
+      }),
+    );
+  }
+}
