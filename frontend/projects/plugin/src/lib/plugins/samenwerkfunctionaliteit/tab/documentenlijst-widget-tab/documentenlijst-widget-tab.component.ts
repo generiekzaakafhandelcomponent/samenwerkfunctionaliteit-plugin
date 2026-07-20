a83c09c8c3a3b20fbf48cd98a91088f2b851dc12
furkang-risa <Furkan@risa-it.nl>
2026-07-20 11:21:30 +0200

    Add helper text to tab

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/tab/documentenlijst-widget-tab/documentenlijst-widget-tab.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/tab/documentenlijst-widget-tab/documentenlijst-widget-tab.component.ts
@@ -1,9 +1,59 @@
-import { Component } from "@angular/core";
-import { DocumentListComponent } from "../../components/document-list/document-list.component";
+import { Component, inject } from '@angular/core';
+import { DocumentListComponent } from '../../components/document-list/document-list.component';
+import { NgTemplateOutlet } from '@angular/common';
+import { InputModule } from 'carbon-components-angular';
+import { OpenZaakUrlService } from '../../service/open-zaak-url.service';
+import { SwfDocumentService } from '../../service/swf-document.service';
+import { ActivatedRoute } from '@angular/router';
+import { take, tap } from 'rxjs';
 
 @Component({
   templateUrl: `./documentenlijst-widget-tab.component.html`,
-  selector: "swf-documentenlijst-widget-tab",
-  imports: [DocumentListComponent],
+  selector: 'swf-documentenlijst-widget-tab',
+  imports: [DocumentListComponent, NgTemplateOutlet, InputModule],
 })
-export class DocumentenlijstWidgetTabComponent {}
+export class DocumentenlijstWidgetTabComponent {
+  private readonly openZaakUrlService: OpenZaakUrlService =
+    inject(OpenZaakUrlService);
+  private readonly swfDocumentService: SwfDocumentService =
+    inject(SwfDocumentService);
+  private readonly route: ActivatedRoute = inject(ActivatedRoute);
+
+  private openZaakUrl: string = '';
+  protected helperText: string = '';
+
+  ngOnInit() {
+    this.getOpenZaakInfoAndSetHelperText();
+  }
+
+  private getOpenZaakInfoAndSetHelperText() {
+    const documentId = this.swfDocumentService.getParam(
+      this.route,
+      'documentId',
+    );
+
+    this.openZaakUrlService
+      .getOpenZaakInfo(documentId)
+      .pipe(
+        take(1),
+        tap((openZaakInfo) => {
+          this.openZaakUrl = openZaakInfo.baseUrl;
+          console.log(this.openZaakUrl);
+        }),
+        tap(() => {
+          this.setHelperText(this.openZaakUrl);
+        }),
+      )
+      .subscribe();
+  }
+
+  private setHelperText(openZaakUrl: string) {
+    this.helperText =
+      'Documenten die vanuit GZAC naar de Samenwerkfunctionaliteit worden geüpload, worden daarnaast ook in Open Zaak ' +
+      'opgeslagen. De bewaartermijn kan voor het zaaktype dat hier gebruikt worden ingesteld. Dit kan dus verschillen ' +
+      'van de vaste bewaartermijn die de Samenwerkfunctionaliteit aanhoudt.' +
+      'In Open Zaak worden de documenten per actieverzoek, en niet — zoals in de Samenwerkfunctionaliteit — ' +
+      `per samenwerking gegroepeerd. ${openZaakUrl} om de lijst van documenten die zijn ` +
+      'opgeslagen in te zien.';
+  }
+}
