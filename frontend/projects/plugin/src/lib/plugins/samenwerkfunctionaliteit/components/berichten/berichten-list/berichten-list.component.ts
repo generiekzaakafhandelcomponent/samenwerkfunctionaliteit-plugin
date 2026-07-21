6f2d04f47a09eee3855a71a80598cb4c9af46f17
furkang-risa <Furkan@risa-it.nl>
2026-07-21 13:36:28 +0200

    Add berichten-list

---
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/berichten/berichten-list/berichten-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-berichten-list',
+  imports: [],
+  templateUrl: './berichten-list.component.html',
+  styleUrl: './berichten-list.component.css',
+})
+export class BerichtenListComponent {}
