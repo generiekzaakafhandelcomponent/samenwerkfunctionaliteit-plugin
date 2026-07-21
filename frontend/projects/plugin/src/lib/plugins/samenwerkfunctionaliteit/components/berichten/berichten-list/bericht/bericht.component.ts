4aebb039522e5b28f4cf78de34627af84686a53c
furkang-risa <Furkan@risa-it.nl>
2026-07-21 16:36:26 +0200

    Add bericht, use Math.random() to simulate received and sent.

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/berichten/berichten-list/bericht/bericht.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/berichten/berichten-list/bericht/bericht.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { ChatBericht } from '../../../../models/bericht.model';
+import { DatePipe } from '@angular/common';
+import { LayerModule } from 'carbon-components-angular';
 
 @Component({
-  selector: "app-bericht",
-  imports: [],
-  templateUrl: "./bericht.component.html",
-  styleUrl: "./bericht.component.scss",
+  selector: 'swf-bericht',
+  imports: [DatePipe, LayerModule],
+  templateUrl: './bericht.component.html',
+  styleUrl: './bericht.component.scss',
 })
-export class BerichtComponent {}
+export class BerichtComponent {
+  message = input<ChatBericht>();
+  isSender = signal(false);
+
+  ngOnInit() {
+    const random: number = Math.random() * 10;
+    if (random < 5) {
+      this.isSender.set(true);
+    }
+  }
+}
