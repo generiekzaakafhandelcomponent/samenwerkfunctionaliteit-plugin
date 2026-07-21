506e67ccf6a2ea99a7488d38c65c9dd840fb8ebd
Gustaaf-RISA <gustaaf@risa-it.nl>
2026-07-21 09:36:31 +0200

    remove redundant strings in config.

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/config/bericht-notification-config.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/config/bericht-notification-config.ts
@@ -2,13 +2,12 @@ import { BerichtNotification } from "../interface/bericht-notification.interface
 
 export const SuccessNotification: BerichtNotification = {
   type: "success",
-  title: "Verzonden:",
-  message: "Het bericht is succesvol verzonden.",
+  title: "",
+  message: "",
 };
 
 export const ErrorNotification: BerichtNotification = {
   type: "error",
-  title: "Bericht kon niet worden verstuurd:",
-  message:
-    "Er ging iets mis tijdens het verzenden van het bericht. Neem contact op met uw beheerder als dit probleem zich vaker voordoet.",
+  title: "",
+  message: "",
 };
