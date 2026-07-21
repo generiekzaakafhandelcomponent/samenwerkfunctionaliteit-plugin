67659f3b6c38e6ee7fb0d75dde431142c1be6717
furkang-risa <Furkan@risa-it.nl>
2026-07-21 13:26:45 +0200

    Move berichten list to berichten directory

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/berichten/berichten-list/bericht/bericht.component.spec.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/berichten/berichten-list/bericht/bericht.component.spec.ts
@@ -1,8 +1,8 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BerichtComponent } from "./bericht.component";
+import { BerichtComponent } from './bericht.component';
 
-describe("BerichtComponent", () => {
+describe('BerichtComponent', () => {
   let component: BerichtComponent;
   let fixture: ComponentFixture<BerichtComponent>;
 
@@ -16,7 +16,7 @@ describe("BerichtComponent", () => {
     fixture.detectChanges();
   });
 
-  it("should create", () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
