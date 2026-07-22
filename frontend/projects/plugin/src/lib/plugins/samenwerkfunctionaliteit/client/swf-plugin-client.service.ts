2152ef56a94da8fe8624c1203b3676cf6c9880b5
furkang-risa <Furkan@risa-it.nl>
2026-07-22 15:55:05 +0200

    Update client and service

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/swf-plugin-client.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/swf-plugin-client.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { SwfPluginProperties } from '../interface/swf-plugin-properties.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -8,8 +9,8 @@ import { Observable } from 'rxjs';
 export class SwfPluginClient {
   private readonly httpClient: HttpClient = inject(HttpClient);
 
-  getSwfPluginProperties(): Observable<Map<string, string>> {
-    return this.httpClient.get<Map<string, string>>(
+  getSwfPluginProperties(): Observable<SwfPluginProperties> {
+    return this.httpClient.get<SwfPluginProperties>(
       'plugin/samenwerkfunctionaliteit/api/v1/properties',
     );
   }
--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/service/swf-plugin.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/service/swf-plugin.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { SwfPluginClient } from '../client/swf-plugin-client.service';
 import { Observable } from 'rxjs';
+import { SwfPluginProperties } from '../interface/swf-plugin-properties.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,7 @@ import { Observable } from 'rxjs';
 export class SwfPluginService {
   private readonly swfPluginClient: SwfPluginClient = inject(SwfPluginClient);
 
-  getSwfPluginProperties(): Observable<Map<string, string>> {
+  getSwfPluginProperties(): Observable<SwfPluginProperties> {
     return this.swfPluginClient.getSwfPluginProperties();
   }
 }
