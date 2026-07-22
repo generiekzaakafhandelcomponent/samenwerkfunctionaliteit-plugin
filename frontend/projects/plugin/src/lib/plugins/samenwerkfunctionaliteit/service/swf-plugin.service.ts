3739133358212645487646451266991a0a3589bc
furkang-risa <Furkan@risa-it.nl>
2026-07-22 15:42:18 +0200

    Update service

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/service/swf-plugin.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/service/swf-plugin.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { SwfPluginClient } from '../client/swf-plugin-client.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SwfPluginService {
-  constructor() {}
+  private readonly swfPluginClient: SwfPluginClient = inject(SwfPluginClient);
+
+  getSwfPluginProperties(): Observable<Map<string, string>> {
+    return this.swfPluginClient.getSwfPluginProperties();
+  }
 }
