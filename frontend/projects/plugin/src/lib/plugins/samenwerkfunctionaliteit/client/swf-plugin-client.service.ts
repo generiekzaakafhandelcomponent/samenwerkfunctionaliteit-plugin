8a2cbd4b72ace815b28d3d640b40e56b33d87304
furkang-risa <Furkan@risa-it.nl>
2026-07-22 16:16:24 +0200

    Use config URL

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/swf-plugin-client.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/swf-plugin-client.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { SwfPluginProperties } from '../interface/swf-plugin-properties.interface';
+import { SWF_PLUGIN_PROPERTIES_URL } from '../config/swf-plugin-config';
 
 @Injectable({
   providedIn: 'root',
@@ -10,8 +11,6 @@ export class SwfPluginClient {
   private readonly httpClient: HttpClient = inject(HttpClient);
 
   getSwfPluginProperties(): Observable<SwfPluginProperties> {
-    return this.httpClient.get<SwfPluginProperties>(
-      'plugin/samenwerkfunctionaliteit/api/v1/properties',
-    );
+    return this.httpClient.get<SwfPluginProperties>(SWF_PLUGIN_PROPERTIES_URL);
   }
 }
