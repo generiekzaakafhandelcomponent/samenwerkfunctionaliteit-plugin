4e8b7668afcfdf4b7f3350e658be0013916dcbd6
furkang-risa <Furkan@risa-it.nl>
2026-07-22 15:36:36 +0200

    Update client to getpluginproperties

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/swf-plugin-client.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/swf-plugin-client.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SwfPluginClientService {
-  constructor() {}
+  private readonly httpClient: HttpClient = inject(HttpClient);
+
+  getSwfPluginProperties(): Observable<Map<string, string>> {
+    return this.httpClient.get<Map<string, string>>(
+      'plugin/samenwerkfunctionaliteit/api/v1/properties',
+    );
+  }
 }
