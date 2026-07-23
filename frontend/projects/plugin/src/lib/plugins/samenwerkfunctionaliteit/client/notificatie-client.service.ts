38d26221d91240e0f008dae86f85c8cff463c8f9
Axel Plowman <axel@risa-it.nl>
2026-07-23 18:02:40 +0200

    Formatting

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/notificatie-client.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/notificatie-client.service.ts
@@ -1,7 +1,11 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { NotificatieResponse } from '../dto/notificatie.dto';
 import { NOTIFICATIES_URL } from '../config/swf-plugin-config';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificatieClient {
   private readonly http: HttpClient = inject(HttpClient);
