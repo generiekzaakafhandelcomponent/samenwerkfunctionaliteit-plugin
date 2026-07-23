50eff09eba8f913260a95e91432c29a0262052ea
furkang-risa <Furkan@risa-it.nl>
2026-07-23 10:51:37 +0200

    Add getActieverzoek to samenwerking client

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/samenwerking-client.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/samenwerking-client.service.ts
@@ -1,16 +1,28 @@
-import {inject, Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {Samenwerking} from "../dto/samenwerking.dto";
-import { Observable } from "rxjs";
-import { SAMENWERKINGEN_URL } from "../config/samenwerking-config";
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Samenwerking } from '../dto/samenwerking.dto';
+import { Observable } from 'rxjs';
+import {
+  ACTIEVERZOEKEN_URL,
+  SAMENWERKINGEN_URL,
+} from '../config/samenwerking-config';
+import { ActieverzoekResponse } from '../dto/actieverzoek.dto';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SamenwerkingClient {
   private readonly http: HttpClient = inject(HttpClient);
 
   getSamenwerking(samenwerkingId: string): Observable<Samenwerking> {
-    return this.http.get<Samenwerking>(`/${SAMENWERKINGEN_URL}/${samenwerkingId}`)
+    return this.http.get<Samenwerking>(
+      `/${SAMENWERKINGEN_URL}/${samenwerkingId}`,
+    );
+  }
+
+  getActieverzoek(actieverzoekId: string): Observable<ActieverzoekResponse> {
+    return this.http.get<ActieverzoekResponse>(
+      `${ACTIEVERZOEKEN_URL}/${actieverzoekId}`,
+    );
   }
 }
