51455f01c3d2502a9f5d4c57fbdb2896d96bb263
Axel Plowman <axel@risa-it.nl>
2026-07-23 15:17:52 +0200

    Create client

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/document-client.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/document-client.service.ts
@@ -1,7 +1,8 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { DocumentenOverzichtResponse } from '../dto/document.dto';
+import { UUID } from '../types/uuid.type';
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +16,16 @@ export class DocumentClient {
   ): Observable<DocumentenOverzichtResponse> {
     return this.http.get<DocumentenOverzichtResponse>(
       `samenwerkfunctionaliteit/v5/samenwerkingen/${samenwerkingId}/${this.DOCUMENTEN_URL}`,
+    );
+  }
+
+  downloadDocument(documentId: UUID): Observable<HttpResponse<Blob>> {
+    return this.http.get(
+      `samenwerkfunctionaliteit/v5/documenten/${documentId}/content`,
+      {
+        observe: 'response',
+        responseType: 'blob',
+      },
     );
   }
 }
