07929b0504a2c3c1172dd6987787a2ebdced6a8e
Axel Plowman <axel@risa-it.nl>
2026-07-23 17:32:52 +0200

    Add translations

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/i18n/nl.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/i18n/nl.ts
@@ -14,5 +14,15 @@ export const pluginNlTranslations = {
       confidentialityType: 'Vertrouwelijkheidsaanduiding',
       dateCreated: 'Datum aangemaakt',
     },
+    userFeedback: {
+      message: {
+        contactYourAdmin:
+          'Neem contact op met uw beheerder als dit probleem zich vaker voordoet.',
+        failedGeneric: 'Er ging iets mis',
+        failedToDownload: 'Er ging iets mis tijdens het downloaden',
+        failedToFetchDocuments:
+          'Er ging iets mis tijdens het ophalen van de lijst van documenten',
+      },
+    },
   },
 };
