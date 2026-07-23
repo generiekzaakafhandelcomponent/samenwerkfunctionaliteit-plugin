6ce4c29600e7ca2e1d221ddaaf3d39273781071f
Axel Plowman <axel@risa-it.nl>
2026-07-23 18:04:07 +0200

    Formatting

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/berichten-client.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/berichten-client.service.ts
@@ -1,18 +1,23 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
-import { PostBerichtRequestDto } from "../dto/post-bericht-request.dto";
-import { PostBerichtResponseDto } from "../dto/post-bericht-response.dto";
-import { Observable } from "rxjs";
-import { ACTIEVERZOEKEN_URL } from "../config/swf-plugin-config";
-
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { PostBerichtRequestDto } from '../dto/post-bericht-request.dto';
+import { PostBerichtResponseDto } from '../dto/post-bericht-response.dto';
+import { Observable } from 'rxjs';
+import { ACTIEVERZOEKEN_URL } from '../config/swf-plugin-config';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BerichtenClient {
   private readonly http: HttpClient = inject(HttpClient);
 
-  postBericht(actieverzoekId: string, bericht: PostBerichtRequestDto): Observable<PostBerichtResponseDto> {
-    return this.http.post<PostBerichtResponseDto>(`/${ACTIEVERZOEKEN_URL}/${actieverzoekId}/berichten`, bericht)
+  postBericht(
+    actieverzoekId: string,
+    bericht: PostBerichtRequestDto,
+  ): Observable<PostBerichtResponseDto> {
+    return this.http.post<PostBerichtResponseDto>(
+      `/${ACTIEVERZOEKEN_URL}/${actieverzoekId}/berichten`,
+      bericht,
+    );
   }
 }
