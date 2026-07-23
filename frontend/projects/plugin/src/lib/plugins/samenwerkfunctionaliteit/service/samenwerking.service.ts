5714de2e18b9b5512d5d4f689f40e7b1e49425f6
furkang-risa <Furkan@risa-it.nl>
2026-07-23 10:52:41 +0200

    Add getActieverzoek to samenwerking service

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/service/samenwerking.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/service/samenwerking.service.ts
@@ -1,15 +1,29 @@
-import {inject, Injectable} from "@angular/core";
-import {SamenwerkingClient} from "../client/samenwerking-client.service";
-import {Observable} from "rxjs";
-import {Samenwerking} from "../models/samenwerking.model";
+import { inject, Injectable } from '@angular/core';
+import { SamenwerkingClient } from '../client/samenwerking-client.service';
+import { Observable } from 'rxjs';
+import { Samenwerking } from '../models/samenwerking.model';
+import { Actieverzoek } from '../models/actieverzoek.model';
+import {
+  ActieverzoekResponse,
+  mapActieverzoekResponseToActieverzoek,
+} from '../dto/actieverzoek.dto';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class SamenwerkingService {
   samenwerkingClient = inject(SamenwerkingClient);
 
   getSamenwerking(samenwerkingId: string): Observable<Samenwerking> {
     return this.samenwerkingClient.getSamenwerking(samenwerkingId).pipe();
+  }
+
+  getActieverzoek(actieverzoekId: string): Observable<Actieverzoek> {
+    return this.samenwerkingClient.getActieverzoek(actieverzoekId).pipe(
+      map((actieverzoekResponse: ActieverzoekResponse) => {
+        return mapActieverzoekResponseToActieverzoek(actieverzoekResponse);
+      }),
+    );
   }
 }
