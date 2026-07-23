4b248c881ece8a96462864829767577915068ea8
Axel Plowman <axel@risa-it.nl>
2026-07-23 18:02:29 +0200

    Update imports

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/document-client.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/document-client.service.ts
@@ -5,25 +5,28 @@ import { DocumentenOverzichtResponse } from '../dto/document.dto';
 import { UUID } from '../types/uuid.type';
 import { FileResponseUtil } from '../utils/file-response.util';
 import { FileDownload } from '../interface/file-download.interface';
+import {
+  SAMENWERKINGEN_URL,
+  DOCUMENTEN_URL,
+} from '../config/swf-plugin-config';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DocumentClient {
   private readonly http: HttpClient = inject(HttpClient);
-  private readonly DOCUMENTEN_URL = 'documenten';
 
   getDocumenten(
     samenwerkingId: string,
   ): Observable<DocumentenOverzichtResponse> {
     return this.http.get<DocumentenOverzichtResponse>(
-      `samenwerkfunctionaliteit/v5/samenwerkingen/${samenwerkingId}/${this.DOCUMENTEN_URL}`,
+      `${SAMENWERKINGEN_URL}}/${samenwerkingId}/documenten`,
     );
   }
 
   downloadDocument(documentId: UUID): Observable<FileDownload> {
     return this.http
-      .get(`samenwerkfunctionaliteit/v5/documenten/${documentId}/content`, {
+      .get(`${DOCUMENTEN_URL}/${documentId}/content`, {
         observe: 'response',
         responseType: 'blob',
       })
