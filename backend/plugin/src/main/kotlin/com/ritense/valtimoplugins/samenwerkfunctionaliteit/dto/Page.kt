c27db4e7cbd222dfd50dd90534e3e1d3a9be4274
furkang-risa <Furkan@risa-it.nl>
2026-07-24 11:43:03 +0200

    Add page dto

---
+++ backend/plugin/src/main/kotlin/com/ritense/valtimoplugins/samenwerkfunctionaliteit/dto/Page.kt
@@ -0,0 +1,8 @@
+package com.ritense.valtimoplugins.samenwerkfunctionaliteit.dto
+
+data class Page(
+    val number: Int,
+    val size: Int,
+    val totalElements: Int,
+    val totalPages: Int,
+)
