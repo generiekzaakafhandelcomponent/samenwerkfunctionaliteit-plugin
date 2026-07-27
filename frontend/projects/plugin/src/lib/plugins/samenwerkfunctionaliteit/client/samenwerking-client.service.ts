b72515b9b06a0cfca788bbd3adfc76999f765e45
furkang-risa <Furkan@risa-it.nl>
2026-07-27 10:11:08 +0200

    Remove the getActieverzoek from the samenwerking client

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/samenwerking-client.service.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/client/samenwerking-client.service.ts
@@ -2,11 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Samenwerking } from '../dto/samenwerking.dto';
 import { Observable } from 'rxjs';
-import {
-  ACTIEVERZOEKEN_URL,
-  SAMENWERKINGEN_URL,
-} from '../config/samenwerking-config';
-import { ActieverzoekResponse } from '../dto/actieverzoek.dto';
+import { SAMENWERKINGEN_URL } from '../config/samenwerking-config';
 
 @Injectable({
   providedIn: 'root',
@@ -17,12 +13,6 @@ export class SamenwerkingClient {
   getSamenwerking(samenwerkingId: string): Observable<Samenwerking> {
     return this.http.get<Samenwerking>(
       `/${SAMENWERKINGEN_URL}/${samenwerkingId}`,
-    );
-  }
-
-  getActieverzoek(actieverzoekId: string): Observable<ActieverzoekResponse> {
-    return this.http.get<ActieverzoekResponse>(
-      `${ACTIEVERZOEKEN_URL}/${actieverzoekId}`,
     );
   }
 }
