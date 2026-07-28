fc9138e71c172839db16a709b39f1b46715faa21
furkang-risa <Furkan@risa-it.nl>
2026-07-28 13:59:06 +0200

    Change actieverzoekstatus types to const

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/types/actieverzoek-status.type.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/types/actieverzoek-status.type.ts
@@ -1,7 +1,11 @@
-export type ActieverzoekStatus =
-  | 'OPEN'
-  | 'IN_BEHANDELING'
-  | 'GEWEIGERD'
-  | 'INGETROKKEN'
-  | 'GEREEDGEMELD'
-  | 'GEREED';
+export const ActieverzoekStatusTypes = {
+  OPEN: 'OPEN',
+  IN_PROGRESS: 'IN_PROGRESS',
+  REJECTED: 'REJECTED',
+  WITHDRAWN: 'WITHDRAWN',
+  REPORTED_READY: 'REPORTED_READY',
+  READY: 'READY',
+} as const;
+
+export type ActieverzoekStatusType =
+  (typeof ActieverzoekStatusTypes)[keyof typeof ActieverzoekStatusTypes];
