8604f412ff31f7c567c823c1c8aa608f69bc25b5
Axel Plowman <axel@risa-it.nl>
2026-08-05 16:19:20 +0200

    Make protected

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/document-list/document-list.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/document-list/document-list.component.ts
@@ -59,7 +59,7 @@ export class DocumentListComponent implements OnInit {
     this.fetchDocumenten(documentId);
   }
 
-  onFileSelected(event: Event): void {
+  protected onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
 
     if (!input.files?.length) {
