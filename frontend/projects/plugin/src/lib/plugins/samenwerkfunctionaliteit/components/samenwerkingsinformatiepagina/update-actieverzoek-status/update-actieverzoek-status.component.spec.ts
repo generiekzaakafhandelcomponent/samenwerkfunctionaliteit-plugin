72db333e8835ff3c1f7007b4853ef39774691bff
furkang-risa <Furkan@risa-it.nl>
2026-08-07 16:06:12 +0200

    Fix test

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/samenwerkingsinformatiepagina/update-actieverzoek-status/update-actieverzoek-status.component.spec.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/samenwerkingsinformatiepagina/update-actieverzoek-status/update-actieverzoek-status.component.spec.ts
@@ -1,5 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UpdateActieverzoekStatusComponent } from './update-actieverzoek-status.component';
+import { HttpClient } from '@angular/common/http';
+import { Actieverzoek } from '../../../models/actieverzoek.model';
+import {
+  ActieverzoekStatusType,
+  ActieverzoekStatusTypes,
+} from '../../../types/actieverzoek-status.type';
+
+const mockActieverzoekStatusTypes: ActieverzoekStatusType[] = [];
+const mockActieverzoek: Actieverzoek = {
+  actieverzoekId: '',
+  amountOfMessages: 0,
+  createdOn: new Date(),
+  description: '',
+  documents: [],
+  lastChangedBy: '',
+  lastChangedByName: '',
+  lastChangedDateTime: '',
+  links: null,
+  notice: '',
+  productId: '',
+  receiver: '',
+  receiverName: '',
+  samenwerkingId: '',
+  sender: '',
+  senderName: '',
+  status: ActieverzoekStatusTypes.OPEN,
+  title: '',
+};
 
 describe('UpdateActieverzoekStatusComponent', () => {
   let component: UpdateActieverzoekStatusComponent;
@@ -8,9 +36,17 @@ describe('UpdateActieverzoekStatusComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [UpdateActieverzoekStatusComponent],
+      providers: [{ provide: HttpClient, useValue: {} }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(UpdateActieverzoekStatusComponent);
+
+    fixture.componentRef.setInput('actieverzoek', mockActieverzoek);
+    fixture.componentRef.setInput(
+      'actieverzoekStatusTypes',
+      mockActieverzoekStatusTypes,
+    );
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
