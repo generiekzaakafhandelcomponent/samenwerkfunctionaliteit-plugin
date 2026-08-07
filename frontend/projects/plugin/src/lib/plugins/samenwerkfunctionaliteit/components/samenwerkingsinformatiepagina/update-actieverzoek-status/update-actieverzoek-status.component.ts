09663f14408ed7cacf0973a932daec3c26f6d09e
furkang-risa <Furkan@risa-it.nl>
2026-08-07 10:35:41 +0200

    Add input

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/samenwerkingsinformatiepagina/update-actieverzoek-status/update-actieverzoek-status.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/samenwerkingsinformatiepagina/update-actieverzoek-status/update-actieverzoek-status.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input, InputSignal } from '@angular/core';
+import { Actieverzoek } from '../../../models/actieverzoek.model';
 
 @Component({
-  selector: 'app-update-actieverzoek-status',
+  selector: 'update-actieverzoek-status',
   imports: [],
   templateUrl: './update-actieverzoek-status.component.html',
-  styleUrl: './update-actieverzoek-status.component.scss'
+  styleUrl: './update-actieverzoek-status.component.scss',
 })
 export class UpdateActieverzoekStatusComponent {
-
+  actieverzoek: InputSignal<Actieverzoek> = input.required<Actieverzoek>();
+  protected readonly JSON = JSON;
 }
