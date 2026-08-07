db164b8b81b2da43ddfda3af9edf9872ec2086cb
furkang-risa <Furkan@risa-it.nl>
2026-08-07 10:03:58 +0200

    add initial component

---
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/samenwerkingsinformatiepagina/update-actieverzoek-status/update-actieverzoek-status.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UpdateActieverzoekStatusComponent } from './update-actieverzoek-status.component';
+
+describe('UpdateActieverzoekStatusComponent', () => {
+  let component: UpdateActieverzoekStatusComponent;
+  let fixture: ComponentFixture<UpdateActieverzoekStatusComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UpdateActieverzoekStatusComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UpdateActieverzoekStatusComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/samenwerkingsinformatiepagina/update-actieverzoek-status/update-actieverzoek-status.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-actieverzoek-status',
+  imports: [],
+  templateUrl: './update-actieverzoek-status.component.html',
+  styleUrl: './update-actieverzoek-status.component.scss'
+})
+export class UpdateActieverzoekStatusComponent {
+
+}
