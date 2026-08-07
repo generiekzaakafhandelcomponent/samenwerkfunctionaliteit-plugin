b64525e171c0ad93a428b4e5b6416bed710810c6
furkang-risa <Furkan@risa-it.nl>
2026-08-07 15:36:25 +0200

    Use cds-label

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/samenwerkingsinformatiepagina/update-actieverzoek-status/actieverzoek-card/actieverzoek-card.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/components/samenwerkingsinformatiepagina/update-actieverzoek-status/actieverzoek-card/actieverzoek-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, input, InputSignal } from '@angular/core';
 import { Actieverzoek } from '../../../../models/actieverzoek.model';
 import { DatePipe } from '@angular/common';
+import { InputModule } from 'carbon-components-angular';
 
 @Component({
   selector: 'actieverzoek-card',
-  imports: [DatePipe],
+  imports: [DatePipe, InputModule],
   templateUrl: './actieverzoek-card.component.html',
   styleUrl: './actieverzoek-card.component.scss',
 })
