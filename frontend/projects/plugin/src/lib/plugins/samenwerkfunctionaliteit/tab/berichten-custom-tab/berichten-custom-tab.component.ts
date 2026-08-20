ee2a0b4c78cfceb500b7a3517164a3b2a5fac137
Axel Plowman <axel@risa-it.nl>
2026-08-20 15:14:46 +0200

    Revert "Fix loading spinner"
    This reverts commit 158600cd2a889c236b885a3780c1acf88c18cc15.

--- frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/tab/berichten-custom-tab/berichten-custom-tab.component.ts
+++ frontend/projects/plugin/src/lib/plugins/samenwerkfunctionaliteit/tab/berichten-custom-tab/berichten-custom-tab.component.ts
@@ -52,13 +52,13 @@ export class BerichtenCustomTabComponent implements OnInit {
     this.iconService.registerAll([Collaborate32]);
     this.getBerichten(this.getBusinessKey());
 
-    this.isLoading.set(true);
+    this.isLoading.set(false);
   }
 
   protected refreshMessages(): void {
     this.isLoading.set(true);
     this.getBerichten(this.getBusinessKey());
-    this.isLoading.set(true);
+    this.isLoading.set(false);
   }
 
   private getBusinessKey(): BusinessKey {
